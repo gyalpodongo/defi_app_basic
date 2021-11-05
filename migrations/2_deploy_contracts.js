13765e92c09bf72199a8b2cdbcff1962c7ded2c3
gyalpo <gyalpodongo@gmail.com>
2021-11-05 17:52:02 -0400

    Added more migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,6 +6,16 @@ const DaiToken = artifacts.require("DaiToken");
 module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(DaiToken);
     const daiToken = await DaiToken.deployed();
-    deployer.deploy(TokenFarm);
+
+    await deployer.deploy(DappToken);
+    const dappToken = await DappToken.deployed()
+
+    await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+    const tokenFarm = await TokenFarm.deployed();
+    
+    await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+    await daiToken.transfer(accounts[1], '1000000000000000000000000')
+
 };
 
