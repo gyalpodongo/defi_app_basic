0fdbba31b427714e7174cde2224b9b6ba1fd7f60
gyalpo <gyalpodongo@gmail.com>
2021-11-05 13:43:21 -0400

    Created new token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,11 @@
 const TokenFarm = artifacts.require("TokenFarm");
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+
+module.exports = async function(deployer, network, accounts) {
+    await deployer.deploy(DaiToken);
+    const daiToken = await DaiToken.deployed();
+    deployer.deploy(TokenFarm);
 };
 
