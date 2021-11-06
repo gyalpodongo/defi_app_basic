4d06468b2e323c300d917ce6ed9b7b0848b18a7a
gyalpo <gyalpodongo@gmail.com>
2021-11-05 20:38:20 -0400

    Added issue tokens function in smart contract

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,6 +5,7 @@ import "./DaiToken.sol";
 
 contract TokenFarm{
     string public name = "Dapp Token Farm";
+    address public owner;
     DappToken public dappToken; 
     DaiToken public daiToken;
 
@@ -16,13 +17,17 @@ contract TokenFarm{
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     // 1. Stakes Tokens (Deposit)
     function stakeTokens(uint _amount) public {
+        // Require amount greater than 0
+        require(_amount > 0, 'ammount cannot be 0 or less');
+
         // Transfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
-
+ 
         //Update staking balance
         stakingBalance[msg.sender] = stakingBalance[msg.sender] +_amount;
 
@@ -39,7 +44,19 @@ contract TokenFarm{
     }
 
 
-    // 2. Unstaking Tokens (Withdraw)
+    //  Unstaking Tokens (Withdraw)
 
-    // 3. Issuing Tokens (Earn interest)
+    //  Issuing Tokens (Earn interest)
+    function issueToken() public {
+        require(msg.sender == owner, "caller must be the owner");
+
+        //Issue tokens to all stakers
+        for (uint i=0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if (balance > 0){
+                dappToken.transfer(recipient,balance);
+            }
+        }
+    }
 }
