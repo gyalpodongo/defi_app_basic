9a512f779f41f267f9753458bf9d1528d6cb9076
gyalpo <gyalpodongo@gmail.com>
2021-11-05 20:11:48 -0400

    Added staking

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,8 +7,39 @@ contract TokenFarm{
     string public name = "Dapp Token Farm";
     DappToken public dappToken; 
     DaiToken public daiToken;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public isStaking;
+    mapping(address => bool) public hasStaked;
+
+    address[] public stakers;
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    // 1. Stakes Tokens (Deposit)
+    function stakeTokens(uint _amount) public {
+        // Transfer Mock Dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        //Update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] +_amount;
+
+        // Add user to stakers array *only* if they haven't stake already
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        //Update Staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+
+
+    }
+
+
+    // 2. Unstaking Tokens (Withdraw)
+
+    // 3. Issuing Tokens (Earn interest)
 }
