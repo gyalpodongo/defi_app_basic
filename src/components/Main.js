1ef0439d83111af16a4e8ecec2ba794e66b7ea52
gyalpo <gyalpodongo@gmail.com>
2021-11-06 10:48:45 -0400

    Added some basic UI

---
+++ src/components/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+
+class Main extends Component {
+
+  render() {
+    return (
+      <div id="content" className="mt-3">
+        <table className="table table-borderless text-muted text-center">
+            <thead>
+                <tr>
+                    <th scope="col">Staking Balance</th>
+                    <th scope="col">Reward Balance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{window.web3.utils.fromWei(this.props.stakingBalance,'Ether')} mDAI</td>
+                    <td>{window.web3.utils.fromWei(this.props.dappTokenBalance,'Ether')} DAPP</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <div className="card mb-4">
+            <div className="card-body">
+                <p>Hello World</p>
+            </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Main;
